bfe0efcfd2ff66a098b29ef319ec16e0d277eed9
Dafrok <o.o@mug.dog>
2017-12-05 14:11:58 +0800

    change public path

--- docs/main.js
+++ docs/main.js
@@ -1,56 +1,32 @@
-import san from 'san'
 import App from './pages/App.san'
-import {router} from 'san-router'
-import Prism from 'prismjs'
-
-// import API from './pages/API.san'
-// import Examples from './pages/Examples.san'
-// import GetStart from './pages/GetStart.san'
 import Jumbotron from './components/Jumbotron.san'
 import Nav from './components/Nav.san'
-// import {transition} from '@/index.js'
 
 import 'prismjs/themes/prism.css'
 import 'normalize.css'
 import './markdown.styl'
 
-router.setMode('html5')
-
 const jumbotron = new Jumbotron()
 const nav = new Nav()
+const app = new App()
 jumbotron.attach(document.body)
 nav.attach(document.body)
+app.attach(document.body)
 
-// const $body = document.createElement('div')
-// document.body.appendChild($body)
+// const $bodyEl = document.createElement('main')
+// document.body.appendChild($bodyEl)
+// const $body = 'main'
 
-const $bodyEl = document.createElement('main')
-document.body.appendChild($bodyEl)
-const $body = 'main'
-
-router.add({
-  rule: '/',
-  Component: App,
-  target: $body
-})
-// .add({
-//   rule: '/start',
-//   Component: GetStart,
-//   target: $body
-// }).add({
-//   rule: '/api',
-//   Component: API,
-//   target: $body
-// }).add({
-//   rule: '/examples',
-//   Component: Examples,
+// router.add({
+//   rule: '/',
+//   Component: App,
 //   target: $body
 // })
 
-router.start()
+// router.start()
 
-router.listen(e => {
-  san.nextTick(() => {
-    Prism.highlightAll()
-  })
-})
+// router.listen(e => {
+//   san.nextTick(() => {
+//     Prism.highlightAll()
+//   })
+// })
